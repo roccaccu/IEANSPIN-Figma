3ce40910dc12452cb4ef5e8095dcafad97c68835
roccaccu <naimadzhun@gmail.com>
2022-06-13 06:54:08 +0800

    third commit

--- src/libs/js/script.js
+++ src/libs/js/script.js
@@ -1,4 +1,21 @@
-var link = document.querySelector("link[rel=import]");
-var content = link.import;
-var linkDoc = content.querySelector(".header");
-document.body.appendChild(content.cloneNode(true));
+// var link = document.querySelector("link[rel=import]");
+// var content = link.import;
+// var linkDoc = content.querySelector(".header");
+// document.body.appendChild(content.cloneNode(true));
+
+// const spinning = document.querySelector('.header-main-img')
+// const spinned = document.querySelector('.spin')
+
+// spinned.addEventListener('click', function () {
+//     document.getElementsByClassName('.header-main-img');
+// })
+
+
+
+const spinThis = document.querySelector('.header-main-img')
+const onSpin = document.querySelector('.spin')
+
+onSpin.addEventListener('click', function () {
+  // burger.style.display = 'none'
+  spinThis.classList.toggle('animate')
+})
